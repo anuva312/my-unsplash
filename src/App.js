7acd2bf4ce3c585eba090a1324be05f0ecd9c6a6
Anusree V A <anusreeva93@gmail.com>
2023-08-29 12:55:16 +0530

    added handling of deletion of image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import MasonryLayout from "./components/MasonryLayout";
 import "react-toastify/dist/ReactToastify.css";
@@ -10,38 +10,38 @@ function App() {
   const [allImages, setAllImages] = useState([]);
   const [filteredImages, setFilteredImages] = useState([]);
 
-  useEffect(() => {
-    const onGetImagesOnSuccess = function (data) {
-      if (data?.images) {
-        data.images.sort(
-          (a, b) => new Date(b.lastUpdatedTime) - new Date(a.lastUpdatedTime)
-        );
-        console.log("images data", data);
-        setAllImages(data.images);
-        setFilteredImages(data.images);
-      }
-    };
-    const getAllImages = async function () {
-      const url = `https://${process.env.REACT_APP_DOMAIN_URL}/api/v1/images`;
-      if (imagesAPIInvoked) return;
+  const onGetImagesSuccess = function (data) {
+    if (data?.images) {
+      data.images.sort(
+        (a, b) => new Date(b.lastUpdatedTime) - new Date(a.lastUpdatedTime)
+      );
+      console.log("images data", data);
+      setAllImages(data.images);
+      setFilteredImages(data.images);
+    }
+  };
+  const getAllImages = useCallback(async () => {
+    const url = `https://${process.env.REACT_APP_DOMAIN_URL}/api/v1/images`;
+    if (imagesAPIInvoked) return;
 
-      imagesAPIInvoked = true;
-      try {
-        const response = await (
-          await fetch(url, {
-            method: "GET",
-            mode: "cors",
-          })
-        ).json();
-        onGetImagesOnSuccess(response.data);
-      } catch (error) {
-        console.error(error);
-        onError("Unable to get images image!");
-      }
-    };
-
-    getAllImages();
+    imagesAPIInvoked = true;
+    try {
+      const response = await (
+        await fetch(url, {
+          method: "GET",
+          mode: "cors",
+        })
+      ).json();
+      onGetImagesSuccess(response.data);
+    } catch (error) {
+      console.error(error);
+      onError("Unable to get images image!");
+    }
   }, []);
+
+  useEffect(() => {
+    getAllImages();
+  }, [getAllImages]);
 
   const onError = function (message) {
     toast.error(message, {
@@ -55,10 +55,19 @@ function App() {
     });
   };
 
+  const onDeleteImageSuccess = function () {
+    onSuccess("Image Deleted Successfully");
+    getAllImages();
+  };
+
   return (
     <div className="my-unsplash">
       <ToastContainer />
-      <MasonryLayout imageList={filteredImages}></MasonryLayout>
+      <MasonryLayout
+        imageList={filteredImages}
+        onDeleteImageSuccess={onDeleteImageSuccess}
+        onDeleteImageError={() => onError("Unable to delete the image")}
+      ></MasonryLayout>
     </div>
   );
 }
